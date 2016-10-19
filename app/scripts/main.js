c668c1e8d1a39a35c87ee9494d79f3ff60894b2a
Michael Perkhofer <michael@perkhofer.at>
2016-10-19 23:55:21 +0200

    Add basic portfolio

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
-console.log('\'Allo \'Allo!');
+var sections = [
+  'quotemachine',
+  'localweather',
+  'calculator',
+  'simon',
+  'tictactoe',
+];
+
+sections.forEach(function(item){
+  $('#' + item).geopattern(item);
+});
+
